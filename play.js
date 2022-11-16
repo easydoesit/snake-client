2eee0cab6823c795a051e0c293227bfac5aadc8b
easydoesit <michael.grills@gmail.com>
2022-11-16 06:07:46 +0000

    add play.js connects to snake and logs when your in and when you end

---
+++ play.js
@@ -0,0 +1,34 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function() {
+  const conn = net. createConnection({
+    host: 'localhost',
+    port: 50541
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf-8");
+
+  conn.on("connect", () => {
+    console.log("You're in! Play the Game!");
+  });
+
+  conn.on("data", (data) => {
+    console.log(data);
+  });
+
+  conn.on("end", () => {
+    console.log("you ded cuz you idled");
+  });
+
+
+
+  return conn;
+
+
+};
+
+console.log("Connecting");
+connect();
+
